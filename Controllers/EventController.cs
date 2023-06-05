83cf5e8b2faa62ec183392928ce391fee6106314
melihafsar <melih_afsar@hotmail.com.tr>
2023-06-05 16:11:13 +0300

    feat: add EventAdder.cshtml, EventView.cshtml, update EventController.cs, some design.

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -15,26 +15,46 @@ public class EventController : Controller
     }
     
     // GET
+    
+    // Tum etkinlikleri listeler
     public IActionResult Events()
     {
         var events = _context.Events.ToList();
         return View(events);
     }
 
+    // Aktif Kullaniciya ait olusturulmus etkinlikleri listeler
     public IActionResult EventsUser()
     {
+        var events = _context.Events.ToList();
         var message = "Events";
         var number = 2;
         ViewData["Message"] = message;
         ViewData["Number"] = number;
         
+
         var array = new[] {"one", "two", "three"};
         ViewData["Array"] = array;
         
-        return (IActionResult) View();
+        return (IActionResult) View(events);
     }
 
+    //Aktif Kullanicinin katildigi - katilacaği etkinlikleri listeler
     public IActionResult EventsJoinUser()
+    {
+        var events = _context.Events.ToList();
+        return View(events);
+    }
+    
+    
+    //Bir etkinlik ayrintilarini gosterir
+    public IActionResult EventView()
+    {
+        return View();
+    }
+    
+    //Etkinlik Ekleme Sayfasini Gosterir
+    public IActionResult EventAdder()
     {
         return View();
     }
